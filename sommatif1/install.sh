70bec71b77fce98ee6ebde94c3bcf0fa016a8bbf
Eva Beaulieu <6315895@cstjean.qc.ca>
2025-09-03 14:05:05 -0400

    fixed file moving

--- sommatif1/install.sh
+++ sommatif1/install.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 su
-cp -T bash.rc ~
-cp -T gitconfig.git ~
-cd ~
-mkdir bin cegep projets
+mkdir ~/bin ~/cegep ~/projets 
+mv .bashrc ~
+mv .gitconfig ~
+mv vimcreate.sh ~/bin
 pacman -S git firefox vim gcc gdb python3 python-pip -y
